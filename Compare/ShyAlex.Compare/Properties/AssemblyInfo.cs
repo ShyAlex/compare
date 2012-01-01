4ccdf54829517f777ec3e07b4097372030bbbce0
Alex Humphrey <alexejhumphrey@hotmail.com>
2012-01-01 12:26:50 +0000

    Updated AssemblyInfos for first release.

--- Compare/ShyAlex.Compare/Properties/AssemblyInfo.cs
+++ Compare/ShyAlex.Compare/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShyAlex.Compare")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Allows structural comparisons of objects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShyAlex.Compare")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © Alex Humphrey 2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.12001")]
+[assembly: AssemblyFileVersion("1.0.0.12001")]
