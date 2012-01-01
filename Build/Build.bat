3217d5d07336e328dcdd855e57dd601cd4b13e0a
Alex Humphrey <alexejhumphrey@hotmail.com>
2012-01-01 12:35:03 +0000

    Updated build and test scripts to accept configuration as a parameter.

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /D %~dp0
 set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
-set config=Debug
+if [%1]==[] (set config=Debug) else (set config=%1)
 %msbuild% ..\Compare\Compare.sln /t:Rebuild /p:Configuration=%config%;Platform="Any CPU"
 exit /b %%ERRORLEVEL%%
--- Build/Test.bat
+++ Build/Test.bat
@@ -1,6 +1,6 @@
 @echo off
 set nunit=..\3rdParty\NUnit\nunit-console-x86.exe
 set opencover=..\3rdParty\OpenCover\OpenCover.Console.exe
-set config=debug
+if [%1]==[] (set config=Debug) else (set config=%1)
 %opencover% -register:user -target:%nunit% -targetargs:""/noshadow" "/xml=ShyAlex.Compare.Tests.xml" "..\Compare\ShyAlex.Compare.Tests\bin\%config%\ShyAlex.Compare.Tests.dll"" -output:ShyAlex.Compare.Coverage.xml -filter:+[ShyAlex.Compare]*
 exit /b %errorlevel%
